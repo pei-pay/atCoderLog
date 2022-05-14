3b9e6e21ef983ae20d8b64a771003cd445bb6cd4
pei-pay <gleektennis@icloud.com>
2022-05-14 12:35:14 +0900

    math_algorithm_book 選択式問題でのランダム回答 期待値

---
+++ math_algorithm_book/3/04/024_answerExamRandomly.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int N, P[59], Q[59];
+double Answer = 0.0;
+
+int main() {
+  cin >> N;
+  for(int i = 0; i< N; i++) cin >> P[i] >> Q[i];
+  for(int i = 0; i < N; i++) {
+    Answer += 1.0 * Q[i] / P[i];
+  }
+  printf("%.12lf\n", Answer);
+  return 0;
+}
