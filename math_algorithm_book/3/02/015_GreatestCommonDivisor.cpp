94f46ba1dd457c473f60cf3704391687797ceef5
pei-pay <gleektennis@icloud.com>
2022-05-22 20:36:16 +0900

    math_algorithm_book 再起関数を用いたユークリッドの互除法

--- math_algorithm_book/3/02/015_GreatestCommonDivisor.cpp
+++ math_algorithm_book/3/02/015_GreatestCommonDivisor.cpp
@@ -14,6 +14,12 @@ long long GCD(long long A, long long B) {
   return B;
 }
 
+// 再起関数を用いたユークリッドの互除法
+long long GCD(long long A, long long B) {
+  if(B == 0 ) return A;
+  return GCD(B, A % B);
+}
+
 int main() {
   long long A, B;
   cin >> A >> B;
