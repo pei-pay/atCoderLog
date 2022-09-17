a698d469b69766064ca760b43f859818e4439f3b
pei-pay <gleektennis@icloud.com>
2022-09-17 10:58:26 +0900

    tweak: .editorconfig

--- math_algorithm_book/5/04/066_threeCards.cpp
+++ math_algorithm_book/5/04/066_threeCards.cpp
@@ -10,12 +10,12 @@ int main() {
   // 事象Aの余事象Bを求める
   long long complementaryEvent = 0;
 
-  for(int a = 1; a <= N; a++) {
-    for(int b = max(1, a-(K-1)); b <= min(N, a+(K-1)); b++) {
-      for(int c = max(1, a-(K-1)); c <= min(N, a+(K-1)); c++) {
-        if(abs(b - c) <= K-1) complementaryEvent += 1;
+  for (int a = 1; a <= N; a++) {
+    for (int b = max(1, a - (K - 1)); b <= min(N, a + (K - 1)); b++) {
+      for (int c = max(1, a - (K - 1)); c <= min(N, a + (K - 1)); c++) {
+        if (abs(b - c) <= K - 1) complementaryEvent += 1;
       }
-    } 
+    }
   }
 
   // 事象Aのパターンの数 = 全体のパターン - 余事象B
