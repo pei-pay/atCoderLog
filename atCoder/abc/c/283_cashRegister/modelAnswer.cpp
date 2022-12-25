f19eb6a6d404caa1ef2383a9e42e6e78d7dd5fc3
pei-pay <gleektennis@icloud.com>
2022-12-25 16:46:07 +0900

    add 283_cashRegister

---
+++ atCoder/abc/c/283_cashRegister/modelAnswer.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for(int i = 0; i < n; i++)
+
+
+int main() {
+  string s;
+  cin >> s;
+  int n = s.size();
+  int ans = 0;
+  for (int i = 0; i < n;) {
+    ans++;
+    if (s[i] == '0' && i + 1 < n && s[i + 1] == '0') {
+      i += 2;
+    }
+    else {
+      i++;
+    }
+  }
+
+  cout << ans << endl;
+  return 0;
+}
+
