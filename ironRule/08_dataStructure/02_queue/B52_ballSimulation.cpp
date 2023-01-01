af1f380b79c0e90bfaea11d3505892a5b39ccdda
pei-pay <gleektennis@icloud.com>
2023-01-01 09:56:19 +0900

    add ballSimulation

--- ironRule/08_dataStructure/02_queue/B52_ballSimulation.cpp
+++ ironRule/08_dataStructure/02_queue/B52_ballSimulation.cpp
@@ -14,12 +14,20 @@ int main() {
   A[X] = '@';
 
   while (!Q.empty()) {
-
+    int pos = Q.front();
+    Q.pop();
+    if (pos - 1 >= 1 && A[pos - 1] == '.') {
+      A[pos - 1] = '@';
+      Q.push(pos - 1);
+    }
+    if (pos + 1 <= N && A[pos + 1] == '.') {
+      A[pos + 1] = '@';
+      Q.push(pos + 1);
+    }
   }
 
-
-
-
+  for (int i = 1; i <= N; i++) cout << A[i];
+  cout << endl;
 
   return 0;
 }
