f0370c766259055bba44f13c411e373d3ed31752
pei-pay <gleektennis@icloud.com>
2023-01-07 13:19:35 +0900

    add map

---
+++ ironRule/08_dataStructure/04_associativeArray/A54_map.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+int Q;
+int QueryType[100009], y[100009];
+string x[100009];
+map<string, int> Map;
+
+int main() {
+  cin >> Q;
+  for (int i = 1; i <= Q; i++) {
+    cin >> QueryType[i];
+    if (QueryType[i] == 1) cin >> x[i] >> y[i];
+    if (QueryType[i] == 2) cin >> x[i];
+  }
+  for (int i = 1; i <= Q; i++) {
+    if (QueryType[i] == 1) Map[x[i]] = y[i];
+    if (QueryType[i] == 2) cout << Map[x[i]] << endl;
+  }
+
+  return 0;
+}
