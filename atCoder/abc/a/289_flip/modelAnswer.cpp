7624860fecf3fd2a1c06e0236acca2e442a581ef
pei-pay <gleektennis@icloud.com>
2023-02-12 10:57:52 +0900

    add: 289_a_flip

---
+++ atCoder/abc/a/289_flip/modelAnswer.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+
+  for (char c : s)
+    if (c == '0')
+      cout << '1';
+    else
+      cout << '0';
+
+  cout << endl;
+
+  return 0;
+}
