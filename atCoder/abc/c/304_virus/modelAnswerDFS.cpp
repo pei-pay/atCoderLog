5828f574e03aefe71ebd7b72b5e6652834374d08
pei-pay <gleektennis@icloud.com>
2023-06-10 15:12:53 +0900

    add: model answer DFS 304 c

---
+++ atCoder/abc/c/304_virus/modelAnswerDFS.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+#define rep1(i, n) for(int i = 1; i <= (n); ++i)
+
+long long n, d;
+vector<long long> x, y;
+
+
+bool is_connected(int i, int j) {
+  long long dis = (x[i] - x[j]) * (x[i] - x[j]) + (y[i] - y[j]) * (y[i] - y[j]);
+  return dis <= d * d;
+}
+
+void dfs(int v, vector<bool>& seen) {
+  seen[v] = true;
+  rep(v2, n) {
+    if (seen[v2]) continue;
+    if (is_connected(v, v2)) dfs(v2, seen);
+  }
+}
+
+int main() {
+  cin >> n >> d;
+  x.resize(n), y.resize(n);
+
+  rep(i, n) cin >> x[i] >> y[i];
+
+
+  vector<bool> seen(n, false);
+  dfs(0, seen);
+
+
+  rep(i, n) {
+    cout << (seen[i] ? "Yes" : "No") << endl;
+  }
+
+  return 0;
+}
