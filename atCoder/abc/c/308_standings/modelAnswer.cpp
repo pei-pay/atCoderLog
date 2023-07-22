59786cf750622694925c16509e0a4318e9486b3a
pei-pay <gleektennis@icloud.com>
2023-07-22 14:49:47 +0900

    add: comments

--- atCoder/abc/c/308_standings/modelAnswer.cpp
+++ atCoder/abc/c/308_standings/modelAnswer.cpp
@@ -15,15 +15,18 @@ int main() {
     ab.emplace_back(a, a + b);
   }
 
+  // [0, 1, 2, ... N] の配列を作成
   vector<int> p(N);
   iota(p.begin(), p.end(), 0);
 
+  // ソートの比較関数 (abの要素を比較してpを並び替える)
   auto f = [&](int i, int j) {
     auto [ai, aj] = ab[i];
     auto [bi, bj] = ab[j];
     return (long long)ai * bj > (long long)aj * bi;
     };
 
+  // NOTE: 比較関数が同率の場合ソートの前後で順序が入れ替わってしまわないように安定ソートを使う  https://ja.wikipedia.org/wiki/%E5%AE%89%E5%AE%9A%E3%82%BD%E3%83%BC%E3%83%88
   stable_sort(p.begin(), p.end(), f);
 
   rep(i, N) {
