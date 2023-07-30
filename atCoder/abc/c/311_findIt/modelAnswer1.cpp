d5388ae56e334b73d1d04f24dbb56a1bc79b0c1d
pei-pay <gleektennis@icloud.com>
2023-07-30 20:53:28 +0900

    add: comments

--- atCoder/abc/c/311_findIt/modelAnswer1.cpp
+++ atCoder/abc/c/311_findIt/modelAnswer1.cpp
@@ -10,19 +10,25 @@ int main() {
   rep(i, n) cin >> a[i + 1];
 
   vector<int> id(n + 1);
-  int k = 1;
-  int v = 1;
+  int k = 1; // 地点vに何番目に訪れているか
+  int v = 1; // k番目に訪れた地点v
+
+  // サイクルを見つけるまでループを回す
   while (id[v] == 0) {
     id[v] = k;
     k++;
     v = a[v];
   }
+
+  // サイクルの地点を配列ansに格納
   vector<int> ans;
-  int len = k - id[v];
+  int len = k - id[v]; // len: サイクルの頂点数
   rep(i, len) {
     ans.push_back(v);
     v = a[v];
   }
+
+  // 出力
   cout << ans.size() << endl;
   for (int v : ans) cout << v << ' ';
   cout << endl;
