155caa4a4c32cc036b81eaf69302e9fa5adc07a2
pei-pay <gleektennis@icloud.com>
2023-08-05 18:37:00 +0900

    solved: abc 312 a

---
+++ atCoder/abc/a/312_chord/myAnswer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+  if (s == "ACE" || s == "BDF" || s == "CEG" || s == "DFA" || s == "EGB" || s == "FAC" || s == "GBD") {
+    cout << "Yes" << endl;
+  }
+  else {
+    cout << "No" << endl;
+  }
+
+  return 0;
+}
