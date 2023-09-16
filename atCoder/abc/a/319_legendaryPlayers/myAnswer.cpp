1dba8111e83266bd091495ffca0e4f2f9d9fe95d
pei-pay <gleektennis@icloud.com>
2023-09-16 16:25:01 +0900

    solved: abc 319 a

---
+++ atCoder/abc/a/319_legendaryPlayers/myAnswer.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+
+/**
+ *
+ * 以下のコードで生成
+ * ```cpp
+ * string s[19], x[19];
+ *  rep(i, 10) {
+ *    cin >> s[i] >> x[i];
+ *  }
+ *  rep(i, 10) {
+ *    cout << "if (s == \"" << s[i] << "\") cout << " << x[i] << " << endl;" << endl;
+ *  }
+ * ```
+*/
+int main() {
+  string s;
+  cin >> s;
+  if (s == "tourist") cout << 3858 << endl;
+  if (s == "ksun48") cout << 3679 << endl;
+  if (s == "Benq") cout << 3658 << endl;
+  if (s == "Um_nik") cout << 3648 << endl;
+  if (s == "apiad") cout << 3638 << endl;
+  if (s == "Stonefeang") cout << 3630 << endl;
+  if (s == "ecnerwala") cout << 3613 << endl;
+  if (s == "mnbvmar") cout << 3555 << endl;
+  if (s == "newbiedmy") cout << 3516 << endl;
+  if (s == "semiexp") cout << 3481 << endl;
+  return 0;
+}
