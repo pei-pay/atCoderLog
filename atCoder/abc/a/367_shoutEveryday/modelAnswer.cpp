3b94884ee27ac75aa7be03308fbb6bd0c403ee7a
pei-pay <gleektennis@icloud.com>
2024-08-28 23:54:02 +0900

    add: model answers 367 a

---
+++ atCoder/abc/a/367_shoutEveryday/modelAnswer.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, b, c;
+  cin >> a >> b >> c;
+  if(b < c) {
+    if(b < a && a < c) {
+      cout << "No" << endl;
+    } else {
+      cout << "Yes" << endl;
+    }
+  } else {
+    if (c < a && a < b) {
+      cout << "Yes" << endl;
+    } else {
+      cout << "No" << endl;
+    }
+  }
+
+  return 0;
+}
