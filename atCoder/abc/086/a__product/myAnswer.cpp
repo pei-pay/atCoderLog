89c0fa5a6d55173eab70872fe4b3f706ababfb20
pei-pay <gleektennis@icloud.com>
2025-02-07 23:46:28 +0900

    solved: abc 086 a

---
+++ atCoder/abc/086/a__product/myAnswer.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+
+  if ((a * b) % 2 == 0) {
+    cout << "Even" << endl;
+  }
+  else {
+    cout << "Odd" << endl;
+  }
+
+  return 0;
+}
