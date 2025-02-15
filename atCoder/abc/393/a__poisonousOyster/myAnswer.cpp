c7d2b347e0dfbd5be3a7492896f829e7c40b166b
pei-pay <gleektennis@icloud.com>
2025-02-15 22:46:34 +0900

    solved: abc 393 a

---
+++ atCoder/abc/393/a__poisonousOyster/myAnswer.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string s1, s2;
+  cin >> s1 >> s2;
+
+  string fine = "fine";
+  string sick = "sick";
+  if (s1 == fine && s2 == fine) {
+    cout << 4 << endl;
+  }
+  else if (s1 == fine && s2 == sick) {
+    cout << 3 << endl;
+  }
+  else if (s1 == sick && s2 == fine) {
+    cout << 2 << endl;
+  }
+  else {
+    cout << 1 << endl;
+  }
+
+  return 0;
+}
