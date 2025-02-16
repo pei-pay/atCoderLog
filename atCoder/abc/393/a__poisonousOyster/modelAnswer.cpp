faabb876d0fd19c291b252bff898dfe781d2a617
pei-pay <gleektennis@icloud.com>
2025-02-16 12:26:02 +0900

    add: model answer 393 a

---
+++ atCoder/abc/393/a__poisonousOyster/modelAnswer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string s1, s2;
+  cin >> s1 >> s2;
+  if (s1 == "sick") {
+    if (s2 == "sick") cout << 1 << endl;
+    else cout << 2 << endl;
+  }
+  else {
+    if (s2 == "sick")  cout << 3 << endl;
+    else cout << 4 << endl;
+  }
+  return 0;
+}
