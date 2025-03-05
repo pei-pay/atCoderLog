f343839658b664e4a9ae25717cc40f6424232b4f
pei-pay <gleektennis@icloud.com>
2025-03-05 19:04:47 +0900

    add: comments abc 395 c

--- atCoder/abc/395/c__shortestDuplicateSubArray/modelAnswer.cpp
+++ atCoder/abc/395/c__shortestDuplicateSubArray/modelAnswer.cpp
@@ -9,22 +9,25 @@ int main() {
   vector<int> a(n);
   rep(i, n) cin >> a[i];
 
-  vector<int> cnt(1000005);
-  int mult = 0;
+  vector<int> cnt(1000005); // 各要素の出現回数を記録するカウンター
+  bool mult = false; // 重複があるかどうか
 
-  const int INF = 1001001001;
+  const int INF = 1001001001; // 初期値として大きな数を設定
   int ans = INF;
-  int r = 0;
+  int r = 0; // 右端のインデックス
   rep(l, n) {
-    while (r < n && mult == 0) {
-      cnt[a[r]]++;
-      if (cnt[a[r]] == 2) mult++;
+    // r を動かして重複要素が見つかるまで進める
+    while (r < n && mult == false) {
+      cnt[a[r]]++; // 要素の出現開始をカウント
+      if (cnt[a[r]] == 2) mult = true; // 初めてこの要素が重複した場合、mult を true にする
       r++;
     }
-    if (mult == 0) break;
+    if (!mult) break; // 重複要素がなければ終了
     ans = min(ans, r - l);
-    if (cnt[a[l]] == 2) mult--;
-    cnt[a[l]]--;
+
+    // a[l] を区間から除外する処理
+    if (cnt[a[l]] == 2) mult = false; // a[l] を除外すると、重複が解消される場合は mult を false に戻す
+    cnt[a[l]]--; // a[l] の出現回数を減らす
   }
   if (ans == INF) ans = -1;
   cout << ans << endl;
