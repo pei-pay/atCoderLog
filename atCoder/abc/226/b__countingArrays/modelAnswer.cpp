6b32df01e394df42f35ed89591f371b5e4980893
pei-pay <gleektennis@icloud.com>
2025-03-07 01:02:09 +0900

    add: model answer abc 226 b

---
+++ atCoder/abc/226/b__countingArrays/modelAnswer.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <set>
+#include <vector>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+  set<vector<int>> st;
+  for (int i = 0; i < N; i++) {
+    int L;
+    cin >> L;
+    vector<int> v(L);
+    for (auto& x : v) cin >> x;
+    st.insert(v);
+  }
+  cout << st.size() << "\n";
+}
