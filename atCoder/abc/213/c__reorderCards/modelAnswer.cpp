4631d27c7cdf862d52d41bc40c703e14a3b94db5
pei-pay <gleektennis@icloud.com>
2025-03-13 00:46:35 +0900

    add: model answer abc 213 c

---
+++ atCoder/abc/213/c__reorderCards/modelAnswer.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+using vi = vector<int>;
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+
+vi compress(vi a) {
+  int n = a.size();
+  map<int, int> mp;
+  rep(i, n) mp[a[i]] = 0;
+  int id = 1;
+  for(auto& p : mp) p.second = id++;
+  rep(i, n) a[i] = mp[a[i]];
+  return a;
+}
+
+int main() {
+  int h, w, n;
+  cin >> h >> w >> n;
+  vi a(n), b(n);
+  rep(i, n) cin >> a[i] >> b[i];
+  a = compress(a);
+  b = compress(b);
+  rep(i, n) {
+    printf("%d %d\n", a[i], b[i]);
+  }
+
+
+return 0;
+}
