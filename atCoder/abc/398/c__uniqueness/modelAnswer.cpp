559c468615ffdece41891693bec6d55bd8f4fb19
pei-pay <gleektennis@icloud.com>
2025-03-23 11:50:00 +0900

    add: codes

--- atCoder/abc/398/c__uniqueness/modelAnswer.cpp
+++ atCoder/abc/398/c__uniqueness/modelAnswer.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 #define rep(i, n) for(int i = 0; i < (n); ++i)
-using ll = long long;
 using vi = vector<int>;
 
 int main() {
+  int n;
+  cin >> n;
+  vi a(n);
+  rep(i, n) cin >> a[i];
+  unordered_map<int, int> cnt;
+  rep(i, n) cnt[a[i]]++;
 
+  int mx = -1, ans = -1;
+  rep(i, n) {
+    if (cnt[a[i]] >= 2) continue;
+    if (mx < a[i]) {
+      mx = a[i];
+      ans = i + 1;
+    }
+  }
+  cout << ans << endl;
   return 0;
 }
