cc47a28d8d3976ae6815b85e93fd926fae75ccb5
pei-pay <gleektennis@icloud.com>
2025-03-23 11:39:34 +0900

    add: model answer abc 398 c

---
+++ atCoder/abc/398/c__uniqueness/modelAnswer.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+using ll = long long;
+using vi = vector<int>;
+
+int main() {
+
+  return 0;
+}
