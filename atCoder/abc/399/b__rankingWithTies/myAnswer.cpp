c44d603051e4995a48db109b834d3a42f2f31efb
pei-pay <gleektennis@icloud.com>
2025-04-06 00:05:57 +0900

    solved: abc 399 b

---
+++ atCoder/abc/399/b__rankingWithTies/myAnswer.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+using P = pair<int, int>;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<P> p(n);
+  rep(i, n) {
+    int x;
+    cin >> x;
+    p[i] = { x, i };
+  };
+  sort(p.rbegin(), p.rend());
+  int r = 1;
+  int mx = p[0].first;
+  int c = 1;
+  vector<P> ans;
+  rep(i, n) {
+    if (p[i].first == mx) {
+      ans.push_back({ p[i].second, r });
+      c++;
+    }
+    else {
+      mx = p[i].first;
+      r = c;
+      c++;
+      ans.push_back({ p[i].second, r });
+    }
+  }
+
+  sort(ans.begin(), ans.end());
+  rep(i, n) {
+    cout << ans[i].second << endl;
+  }
+  return 0;
+}
