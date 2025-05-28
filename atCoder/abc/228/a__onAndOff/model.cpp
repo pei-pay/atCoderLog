e632849d93228b83d005fc54da306caea7d5239f
pei-pay <gleektennis@icloud.com>
2025-05-28 22:16:15 +0900

    add: model answer abc 228 a

---
+++ atCoder/abc/228/a__onAndOff/model.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int s, t, x;
+  cin >> s >> t >> x;
+  if (s < t) {
+    cout << (s <= x and x < t ? "Yes" : "No") << '\n';
+  }
+  else {
+    cout << (x < t or s <= x ? "Yes" : "No") << '\n';
+  }
+}
