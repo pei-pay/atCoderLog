d9e17c3db2f5652f5f44376a655b9156b121f058
pei-pay <gleektennis@icloud.com>
2025-08-15 16:32:56 -0500

    model: abc 339 b

---
+++ atCoder/abc/339/b__langtonsTakahashi/model.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+
+const int di[] = {-1, 0, 1, 0};
+const int dj[] = {0, 1, 0, -1};
+
+int main() {
+  int h, w, n;
+  cin >> h >> w >> n;
+  vector<string> s(h, string(w, '.'));
+
+  int i = 0, j = 0, v = 0;
+  rep(ti, n) {
+    if(s[i][j] == '.') {
+      s[i][j] = 'w';
+      v+=1;
+    } else {
+      s[i][j] = '.';
+      v+=3;
+    } 
+    v %= 4;
+    i += di[v]; j += dj[v];
+    i = (i + h) % h;
+    j = (j + w) % w;
+  }
+
+  rep(i, h) cout << s[i] << endl;
+  return 0;
+}
