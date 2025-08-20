0f0703e7450501cd0439c42fbad91c0db460e64c
pei-pay <gleektennis@icloud.com>
2025-08-20 14:44:56 -0500

    model: abc 266 b

---
+++ atCoder/abc/266/b__moduloNumber/model.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+const int mod = 998244353;
+
+int main() {
+  long long n;
+  cin >> n;
+  n %= mod;
+  if (n < 0) n += mod;
+  cout << n << endl;
+  return 0;
+}
