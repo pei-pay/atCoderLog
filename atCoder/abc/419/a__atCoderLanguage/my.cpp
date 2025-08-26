a4aa9c4ae132436b49a3e8b2ecd545f563584332
pei-pay <gleektennis@icloud.com>
2025-08-26 13:31:00 -0500

    solved: abc 419 a

---
+++ atCoder/abc/419/a__atCoderLanguage/my.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+  if(s == "red") cout << "SSS" << endl;
+  else if(s == "blue") cout << "FFF" << endl;
+  else if(s == "green") cout << "MMM" << endl;
+  else cout << "Unknown" << endl;
+  return 0;
+}
