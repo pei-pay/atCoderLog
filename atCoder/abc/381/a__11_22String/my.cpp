1cecaddb34dfb2d09c5e08fe46b8986fcf95c115
pei-pay <gleektennis@icloud.com>
2025-12-11 17:21:31 -0600

    refactor: abc 381 a

--- atCoder/abc/381/a__11_22String/my.cpp
+++ atCoder/abc/381/a__11_22String/my.cpp
@@ -2,31 +2,24 @@
 using namespace std;
 #define rep(i, n) for(int i = 0; i < (n); ++i)
 
+void No() {
+  cout << "No" << endl;
+  exit(0);
+}
+
 int main() {
   int n;
   string s;
   cin >> n;
   cin >> s;
-  if (n % 2 == 0) {
-    cout << "No" << endl;
-    return 0;
-  }
+  if (n % 2 == 0) No();
   int half = n / 2;
-  if (s[half] != '/') {
-    cout << "No" << endl;
-    return 0;
-  }
+  if (s[half] != '/') No();
   rep(i, half) {
-    if (s[i] != '1') {
-      cout << "No" << endl;
-      return 0;
-    }
+    if (s[i] != '1') No();
   }
   for (int i = half + 1; i < n; ++i) {
-    if (s[i] != '2') {
-      cout << "No" << endl;
-      return 0;
-    }
+    if (s[i] != '2') No();
   }
   cout << "Yes" << endl;
   return 0;
