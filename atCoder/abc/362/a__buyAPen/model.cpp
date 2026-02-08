845f28b5a2461eb7ce903d4827e87019ba0f888c
pei-pay <gleektennis@icloud.com>
2026-02-07 23:44:31 -0600

    model: abc 362 a

---
+++ atCoder/abc/362/a__buyAPen/model.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int r, g, b;
+  cin >> r >> g >> b;
+  string c;
+  cin >> c;
+  if (c == "Red") {
+    cout << min(g, b) << endl;
+  }
+  else if (c == "Green") {
+    cout << min(r, b) << endl;
+  }
+  else if (c == "Blue") {
+    cout << min(r, g) << endl;
+  }
+  return 0;
+}
