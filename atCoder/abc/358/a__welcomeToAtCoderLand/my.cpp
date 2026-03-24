e715b8966c972e898ea078badb61bc1b44d237e1
pei-pay <gleektennis@icloud.com>
2026-03-24 15:27:37 -0500

    solved: abc 358 a

---
+++ atCoder/abc/358/a__welcomeToAtCoderLand/my.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string s, t;
+  cin >> s >> t;
+  if (s == "AtCoder" && t == "Land") cout << "Yes" << endl;
+  else cout << "No" << endl;
+  return 0;
+}
